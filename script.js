a1947e9dcb774762df2fc097950ba0e94dfe9aad
xtirachi <168432733+xtirachi@users.noreply.github.com>
2024-07-25 11:00:15 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function startColoringActivity() {
+    alert('Sfinks rəngləmə fəaliyyəti başlayır!');
+    // Add code to initiate the coloring activity
+}
+
+function startPyramidActivity() {
+    alert('Piramida hazırlama fəaliyyəti başlayır!');
+    // Add code to initiate the pyramid activity
+}
+
+// Add more functions for other activities as needed
