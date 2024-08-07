8de35516270282b957d98dcce16bc75bf1f38395
xtirachi <168432733+xtirachi@users.noreply.github.com>
2024-08-07 12:02:44 +0400

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,19 @@
             const timers = document.querySelectorAll('.timer');
             const audio = document.querySelector("audio");
 
-            // Example: Add custom play/pause buttons
-            const playButton = document.getElementById("play-button");
+          const playButton = document.getElementById("play-button");
             const pauseButton = document.getElementById("pause-button");
+
+            if (playButton && pauseButton) {
+                playButton.addEventListener("click", () => {
+                    audio.play();
+                });
+
+                pauseButton.addEventListener("click", () => {
+                    audio.pause();
+                });
+            }
+
 
             playButton.addEventListener("click", () => {
                 audio.play();
