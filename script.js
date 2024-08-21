4d6a6889630279b64f4e4172144191ca87b2aacd
xtirachi <168432733+xtirachi@users.noreply.github.com>
2024-08-21 16:30:20 +0400

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,10 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     uploadButtons.forEach((button) => {
-        button.addEventListener('click', () => uploadFile(fileInput));
+        button.addEventListener('click', () => {
+            const fileInput = button.previousElementSibling; // **Change 2: Get the input element before the button**
+            uploadFile(fileInput); // Pass the correct file input element to the upload function
+        });
     });
 });
 
